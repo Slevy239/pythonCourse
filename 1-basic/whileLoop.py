84ce113f11dcad6691f4d2382a8da9338601c768
Sam Levy <slevy239@gmail.com>
2020-03-04 12:33:22 -0500

    loops

---
+++ 1-basic/whileLoop.py
@@ -0,0 +1,25 @@
+# spam = 0
+# while spam < 5:
+#     print("hello world")
+#     spam = spam + 1
+
+
+# name = ''
+# while name != 'your name':
+#     print('please type your name.')
+#     name = input()
+# print("thank you")
+
+# name = ''
+# while True:
+#     name = input()
+#     if name == 'your name':
+#         break
+# print("thank you")
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print("spam is " + str(spam))
---
+++ ForLoops.py
@@ -0,0 +1,15 @@
+print("My name is")
+for i in range(12, 16):
+    print("jimmy five times " + str(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+# print('my name is')
+# i=0
+# while i < 5:
+#     print("Jimmy 5x " + str(i))
+#     i = i + 1
+
