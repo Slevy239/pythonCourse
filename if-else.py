6c8de28798e6ff5c16e8cdbd01dbd127d407da88
Sam Levy <slevy239@gmail.com>
2020-03-04 09:52:38 -0500

    elif

---
+++ if-else.py
@@ -0,0 +1,9 @@
+print("Enter your Password!")
+userInput = input()
+password = "69"
+if userInput == '':
+    print("NOTHING?!?!")
+elif userInput != password:
+    print("INCORRECT!")
+elif userInput == password:
+    print("NICE.")
