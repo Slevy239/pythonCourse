1585518932e3fb7f4cc7ab58860b1a6514ca7770
Sam Levy <slevy239@gmail.com>
2020-03-04 13:35:23 -0500

    func

---
+++ 2-functions/writing-func.py
@@ -0,0 +1,12 @@
+
+
+name = input('name:')
+userInput = input('password:')
+password = userInput
+
+   if userInput == 'password':
+        print("correct" + name)
+    elif userInput == ' ':
+        print('no')
+    elif userInput != 'wrong':
+        print("wrong again")
